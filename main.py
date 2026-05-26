108e9fe7c7b320d448820e03cd740e76abf5fb61
Neal Nikkam <nealnikkam@gmail.com>
2026-05-26 02:07:58 +0000

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Simple starter code for GitHub Codespaces
+
+def greet(name):
+    return f"Hello, {name}! Welcome to your Codespace."
+
+def add_numbers(a, b):
+    return a + b
+
+# Run the functions
+print(greet("Games"))
+print("2 + 5 =", add_numbers(2, 5))
